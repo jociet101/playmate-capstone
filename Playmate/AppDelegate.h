4f4eeddc7b66149412e1086f5d3600698475018e
Jocelyn Tseng <jocelyntseng@fb.com>
2022-06-29 15:02:16 -0700

    Initial Commit

---
+++ Playmate/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Playmate
+//
+//  Created by Jocelyn Tseng on 6/29/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
