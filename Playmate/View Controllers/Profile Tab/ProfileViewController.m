c109d788f8cf9c6a7d233decb70b10f23f92b44e
Jocelyn Tseng <jocelyntseng@fb.com>
2022-07-05 09:48:45 -0700

    implemented profile image picker and save to parse

--- Playmate/View Controllers/Profile Tab/ProfileViewController.m
+++ Playmate/View Controllers/Profile Tab/ProfileViewController.m
@@ -10,7 +10,7 @@
 #import "SceneDelegate.h"
 #import "DateTools.h"
 
-@interface ProfileViewController ()
+@interface ProfileViewController () <UIImagePickerControllerDelegate>
 
 @property (weak, nonatomic) IBOutlet UIImageView *profileImageView;
 @property (weak, nonatomic) IBOutlet UILabel *nameLabel;
@@ -18,6 +18,7 @@
 @property (weak, nonatomic) IBOutlet UILabel *ageLabel;
 @property (weak, nonatomic) IBOutlet UILabel *genderLabel;
 @property (weak, nonatomic) IBOutlet UILabel *bioField;
+@property (weak, nonatomic) IBOutlet UILabel *profileImagePlaceholder;
 
 @end
 
@@ -32,7 +33,7 @@ NSString *defaultBio = @"Enter a bio.";
     self.nameLabel.text = [user[@"firstName"][0] stringByAppendingString:[@" " stringByAppendingString:user[@"lastName"][0]]];
     self.usernameLabel.text = [@"@" stringByAppendingString:user[@"username"]];
     self.usernameLabel.textColor = [UIColor lightGrayColor];
-    self.genderLabel.text = user[@"gender"][0];
+    self.genderLabel.text = [@"Identifies as " stringByAppendingString:user[@"gender"][0]];
     self.ageLabel.text = [@"Born " stringByAppendingString:[user[@"birthday"][0] timeAgoSinceNow]];
     
     if (![self.bioField.text isEqualToString:defaultBio]) {
@@ -40,9 +41,60 @@ NSString *defaultBio = @"Enter a bio.";
     }
     if (user[@"profileImage"] != nil) {
         // set image stuff
+        UIImage* img = [UIImage imageWithData:[user[@"profileImage"] getData]];
+        [self.profileImageView setImage:img];
+        self.profileImagePlaceholder.alpha = 0;
     }
     
 }
+
+#pragma mark - Uploading or taking profile image
+
+- (void)initializeTaker {
+    UIImagePickerController *imagePickerVC = [UIImagePickerController new];
+    imagePickerVC.delegate = (id)self;
+    imagePickerVC.allowsEditing = YES;
+    
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypeCamera;
+    }
+    else {
+        NSLog(@"Camera 🚫 available so we will use photo library instead");
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    }
+    
+    [self presentViewController:imagePickerVC animated:YES completion:nil];
+}
+
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    
+    // Get the image captured by the UIImagePickerController
+    UIImage *editedImage = info[UIImagePickerControllerEditedImage];
+
+    // Do something with the images (based on your use case)
+    [self.profileImageView setImage:editedImage];
+    self.profileImagePlaceholder.alpha = 0;
+    
+    // save profile image
+    NSData* data = UIImagePNGRepresentation(editedImage);
+    PFFileObject *file = [PFFileObject fileObjectWithData:data];
+    
+    PFUser *user = [PFUser currentUser];
+    user[@"profileImage"] = file;
+    [user saveInBackgroundWithBlock:^(BOOL succeeded, NSError * _Nullable error) {
+        if (succeeded) {
+            NSLog(@"Profile image saved!");
+        } else {
+            NSLog(@"Error: %@", error.description);
+        }
+    }];
+    
+    // Dismiss UIImagePickerController to go back to your original view controller
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+#pragma mark - Handling button or gesture actions
 
 - (IBAction)didTapLogout:(id)sender {
     [PFUser logOutInBackgroundWithBlock:^(NSError * _Nullable error) {
@@ -58,6 +110,9 @@ NSString *defaultBio = @"Enter a bio.";
 - (IBAction)didTapEdit:(id)sender {
 }
 
+- (IBAction)uploadProfileImage:(id)sender {
+    [self initializeTaker];
+}
 
 /*
 #pragma mark - Navigation
