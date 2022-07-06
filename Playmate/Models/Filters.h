76edfafe6c35b8aaf5be87c5730d097e1b43a2f2
Jocelyn Tseng <jocelyntseng@fb.com>
2022-07-06 13:59:23 -0700

    [Filters] designed view controller components on storyboard

---
+++ Playmate/Models/Filters.h
@@ -0,0 +1,18 @@
+//
+//  Filters.h
+//  Playmate
+//
+//  Created by Jocelyn Tseng on 7/6/22.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Filters : NSObject
+
+@property (nonatomic, strong) NSString *sport;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Playmate/Models/Filters.m
@@ -0,0 +1,12 @@
+//
+//  Filters.m
+//  Playmate
+//
+//  Created by Jocelyn Tseng on 7/6/22.
+//
+
+#import "Filters.h"
+
+@implementation Filters
+
+@end
