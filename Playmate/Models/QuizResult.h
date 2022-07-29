c230dc53a9dd7ceab2a72a2adf95f2b19c3e0026
Jocelyn Tseng <jocelyntseng@fb.com>
2022-07-29 13:18:37 -0700

    [Quiz feature] created quiz result model

---
+++ Playmate/Models/QuizResult.h
@@ -0,0 +1,27 @@
+//
+//  QuizResult.h
+//  Playmate
+//
+//  Created by Jocelyn Tseng on 7/29/22.
+//
+
+#import <Parse/Parse.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface QuizResult : PFObject<PFSubclassing>
+
+@property (nonatomic, strong) NSString *userObjectId;
+@property (nonatomic, strong) NSArray *playSportsList;
+@property (nonatomic, strong) NSArray *dontPlaySportsList;
+@property (nonatomic, strong) NSArray *gendersList;
+@property (nonatomic, strong) NSArray *ageGroupsList;
+
++ (void)saveQuizResultWithSports:(NSArray *)sports
+                    andNotSports:(NSArray *)notSports
+                      andGenders:(NSArray *)genders
+                         andAges:(NSArray *)ages;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Playmate/Models/QuizResult.m
@@ -0,0 +1,37 @@
+//
+//  QuizResult.m
+//  Playmate
+//
+//  Created by Jocelyn Tseng on 7/29/22.
+//
+
+#import "QuizResult.h"
+
+@implementation QuizResult
+
+@dynamic userObjectId;
+@dynamic playSportsList;
+@dynamic dontPlaySportsList;
+@dynamic gendersList;
+@dynamic ageGroupsList;
+
++ (nonnull NSString *)parseClassName {
+    return NSStringFromClass([QuizResult class]);
+}
+
++ (void)saveQuizResultWithSports:(NSArray *)sports
+                    andNotSports:(NSArray *)notSports
+                      andGenders:(NSArray *)genders
+                         andAges:(NSArray *)ages {
+    QuizResult *result = [QuizResult new];
+    PFUser *user = [[PFUser currentUser] fetchIfNeeded];
+    result.userObjectId = user.objectId;
+    result.playSportsList = sports;
+    result.dontPlaySportsList = notSports;
+    result.gendersList = genders;
+    result.ageGroupsList = ages;
+    
+    [result saveInBackground];
+}
+
+@end
